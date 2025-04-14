43caa161db238fa489776306529df6e9a1ef438c
FrMks <maxfrancuzov@gmail.com>
2025-04-14 10:52:19 +0300

    Add select image Button

--- RentestWPFTestTask/Infrastructure/Commands/LambdaCommand.cs
+++ RentestWPFTestTask/Infrastructure/Commands/LambdaCommand.cs
@@ -3,10 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using RentestWPFTestTask.Infrastructure.Commands.Base;
 
 namespace RentestWPFTestTask.Infrastructure.Commands
 {
-    internal class LambdaCommand
+    internal class LambdaCommand : Command
     {
+        private readonly Action<object> _Execute;
+        private readonly Func<object, bool> _CanExecute;
+
+        public LambdaCommand(Action<object> Execute, Func<object, bool> CanExecute = null)
+        {
+            _Execute = Execute ?? throw new ArgumentNullException(nameof(Execute));
+            _CanExecute = CanExecute;
+        }
+
+        public override bool CanExecute(object? parameter) => _CanExecute?.Invoke(parameter) ?? true;
+
+        public override void Execute(object? parameter) => _Execute(parameter);
     }
 }
--- RentestWPFTestTask/ViewModels/MainWindowViewModel.cs
+++ RentestWPFTestTask/ViewModels/MainWindowViewModel.cs
@@ -3,10 +3,33 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
+using Microsoft.Win32;
+using RentestWPFTestTask.Infrastructure.Commands;
+using RentestWPFTestTask.ViewModels.Baze;
 
 namespace RentestWPFTestTask.ViewModels
 {
-    internal class MainWindowViewModel
+    internal class MainWindowViewModel : ViewModel
     {
+        public ICommand OpenImageCommand { get; }
+        private bool CanOpenImageCommandExecute(object p) => true;
+        private void OnOpenImageCommandExecute(object p)
+        {
+            var openFileDialog = new OpenFileDialog
+            {
+                Filter = "Image files (*.png;*.jpeg;*.jpg;*.bmp)|*.png;*.jpeg;*.jpg;*.bmp|All files (*.*)|*.*",
+                InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyPictures)
+            };
+            if (openFileDialog.ShowDialog()  == true)
+            {
+                string selectedImagePath = openFileDialog.FileName;
+            }
+        }
+
+        public MainWindowViewModel()
+        {
+            OpenImageCommand = new LambdaCommand(OnOpenImageCommandExecute, CanOpenImageCommandExecute);
+        }
     }
 }
