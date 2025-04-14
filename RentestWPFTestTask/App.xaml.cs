3131096b61704148509907a9f99f99012c01ab60
FrMks <maxfrancuzov@gmail.com>
2025-04-14 10:28:08 +0300

    Create the basic architecture

--- RentestWPFTestTask/App.xaml.cs
+++ RentestWPFTestTask/App.xaml.cs
@@ -4,9 +4,6 @@ using System.Windows;
 
 namespace RentestWPFTestTask
 {
-    /// <summary>
-    /// Interaction logic for App.xaml
-    /// </summary>
     public partial class App : Application
     {
     }
---
+++ RentestWPFTestTask/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RentestWPFTestTask.ViewModels
+{
+    internal class MainWindowViewModel
+    {
+    }
+}
